f2ce700a43cbc5347a2d60282b5f848a1373f4d9
Dangerdash77 <128376561+Dangerdash77@users.noreply.github.com>
2025-07-24 22:49:51 +0530

    a

--- anb-backend/index.js
+++ anb-backend/index.js
@@ -22,9 +22,9 @@ app.use(cookieParser());
 
 // ✅ CORS setup — allows multiple origins (dev & prod)
 const allowedOrigins = [
-  process.env.CLIENT_ORIGIN
-  // "https://www.anbindustries.com",
-  // "http://localhost:5173"
+  // process.env.CLIENT_ORIGIN
+  "https://www.anbindustries.com",
+  "http://localhost:5173"
 ];
 
 app.use(cors({
